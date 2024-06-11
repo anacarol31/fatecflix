43e2f2b59b6bc0ea538321bed36105bd4c15b1b1
Ana <anacarolineofc130@gmail.com>
2024-06-10 22:24:48 -0300

    Atualização feita

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,262 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Filmes Aplicativo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  // Lista de filmes
+  List<Movie> movies = [
+    Movie(
+      title: 'Minha Mãe É Uma Peça O Filme',
+      ano: 2013,
+      genero: 'Comédia',
+      imageUrl: 'assets/images/filme1.jpg',
+      sinopse:'Minha Mãe é uma Peça é uma comédia brasileira que segue a história de Dona Hermínia, uma mãe solteira e engraçada que está enfrentando o desafio de criar seus filhos.',
+      elenco: ['Paulo Gustavo', 'Rodrigo Pandolfo', 'Mariana Xavier', 'Samantha Schmütz'],
+    ),
+    Movie(
+      title: 'A Cabana',
+      ano: 2016,
+      genero: 'Romance',
+      imageUrl: 'assets/images/filme2.jpg',
+      sinopse:'"A Cabana" é um filme sobre Mack, um homem devastado pela perda de sua filha, que recebe uma misteriosa carta de Deus, convidando-o a voltar ao local onde ela foi morta. Lá, ele encontra Deus, Jesus e o Espírito Santo, que o ajudam a enfrentar sua dor e encontrar redenção e cura espiritual.',
+      elenco: ['Sam Worthington', 'Octavia Spencer', 'Tim McGraw'],
+    ),
+    Movie(
+      title: 'Top Gun',
+      ano: 2022,
+      genero: 'Ação e Drama',
+      imageUrl: 'assets/images/filme3.jpg',
+      sinopse: '"Top Gun" segue o piloto Maverick na escola de elite da Marinha, onde compete para ser o melhor e enfrenta a perda de seu amigo Goose, enquanto se envolve com a instrutora Charlie.',
+      elenco: ['Tom Cruise', 'Kelly McGillis', 'Val Kilmer'],
+    ),
+    Movie(
+      title: 'Planeta dos Macacos, o Reinado',
+      ano: 2024,
+      genero: 'Ação, Aventura e Ficção Científica',
+      imageUrl: 'assets/images/filme04.jpg',
+      sinopse:'"Planeta dos Macacos: O Reinado" segue uma nova geração de macacos lutando para sobreviver e formar sociedades após o colapso da civilização humana.',
+      elenco: ['Andy Serkis como César (macaco)', 'Woody Harrelson como Coronel McCullough (humano)', 'Amiah Miller como Nova (humano)'],
+    ),
+    Movie(
+      title: 'Como eu era antes de você',
+      ano: 2016,
+      genero: 'Drama e Romance',
+      imageUrl: 'assets/images/filme5.jpg',
+      sinopse: '"Como Eu Era Antes de Você" conta a história de Lou, uma cuidadora que se apaixona por Will, um homem tetraplégico. Enquanto enfrentam desafios emocionais juntos, eles descobrem o poder do amor e da aceitação.',
+      elenco: ['Emilia Clarke como Louisa Clark', 'Sam Claflin como Will Traynor', 'Janet McTeer como Camilla Traynor'],
+    ),
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String? selectedGenero;
+
+  // Nomes dos integrantes
+  List<String> integrantes = [
+    'Ana Caroline',
+    'Matheus Conceição',
+    'Nikoly Pereira',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    List<Movie> filteredMovies = selectedGenero != null
+        ? movies.where((movie) => movie.genero == selectedGenero).toList()
+        : movies;
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Filmes'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          DropdownButton<String>(
+            hint: Text('Filtro'),
+            value: selectedGenero,
+            onChanged: (String? value) {
+              setState(() {
+                selectedGenero = value!;
+              });
+            },
+            items: _getGeneros().map<DropdownMenuItem<String>>((genero) {
+              return DropdownMenuItem<String>(
+                value: genero,
+                child: Text(genero),
+              );
+            }).toList(),
+          ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // Duas colunas
+                childAspectRatio: 0.9, // Proporção entre altura e largura
+              ),
+              itemCount: filteredMovies.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 4,
+                  margin: EdgeInsets.all(8),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MovieDetailsPage(movie: filteredMovies[index]),
+                        ),
+                      );
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.vertical(top: Radius.circular(4)),
+                            child: Image.network(
+                              filteredMovies[index].imageUrl,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                filteredMovies[index].title,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(filteredMovies[index].ano.toString()),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Integrantes'),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: integrantes.map((integrante) {
+                        return Text(integrante,
+                        style:TextStyle(fontWeight: FontWeight.bold));
+                      }).toList(),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Fechar'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: Text('Integrantes'),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  List<String> _getGeneros() {
+    Set<String> generos = movies.map((movie) => movie.genero).toSet();
+    return List<String>.from(generos);
+  }
+}
+
+class MovieDetailsPage extends StatelessWidget {
+  final Movie movie;
+
+  const MovieDetailsPage({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movie.title),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.network(
+                movie.imageUrl,
+                width: 300,///detalhes
+                height: 420,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 20),
+              Text('Ano: ${movie.ano}'),
+              Text('Gênero: ${movie.genero}'),
+              Text('Sinopse: ${movie.sinopse}'),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Elenco:'),
+                  Expanded(
+                    child: Text(
+                      movie.elenco.join(','),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Movie {
+  final String title;
+  final int ano;
+  final String genero;
+  final String imageUrl;
+  final String sinopse;
+  final List<String> elenco;
+
+  Movie({
+    required this.title,
+    required this.ano,
+    required this.genero,
+    required this.imageUrl,
+    required this.sinopse,
+    required this.elenco,
+  });
 }
